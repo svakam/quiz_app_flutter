b700b05a5529978b419add2cc41a634ad1dc98b5
vikra <vikramakam@hotmail.com>
2026-05-10 11:37:53 -0700

    initialized app + created quiz class model
    Signed-off-by: vikra <vikramakam@hotmail.com>

---
+++ lib/models/quiz.dart
@@ -0,0 +1,31 @@
+// each question = prompt + correct ans + list of incorrect ans
+class Question {
+  final String prompt; // class variables
+  final String correctAnswer;
+  final List<String> incorrectAnswers;
+
+  const Question({ // constructor
+    required this.prompt,
+    required this.correctAnswer,
+    required this.incorrectAnswers,
+  });
+
+  List<String> get allAnswers => [correctAnswer, ...incorrectAnswers];
+}
+
+// each quiz holds time, id, title, description of quiz, and question list
+class Quiz {
+  final String id;
+  final String title;
+  final String description;
+  final DateTime createdAt;
+  final List<Question> questions;
+
+  const Quiz({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.createdAt,
+    required this.questions,
+  });
+}
